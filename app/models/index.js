ab6ee775989377dc3095bef12fe72d4611c08a17
Pat Ranile <fatimaranile@gmail.com>
2021-07-26 21:31:19 +0800

    Initialize sequelize.

---
+++ app/models/index.js
@@ -0,0 +1,29 @@
+const dbConfiguration = require("../config/db.config.js");
+const Sequelize = require("sequelize");
+
+const sequelize = new Sequelize(
+  dbConfiguration.DB,
+  dbConfiguration.USER,
+  dbConfiguration.PASSWORD,
+  {
+    host: dbConfiguration.HOST,
+    dialect: dbConfiguration.dialect,
+    operatorAliases: false,
+
+    pool: {
+      max: dbConfiguration.pool.max,
+      min: dbConfiguration.pool.min,
+      acquire: dbConfiguration.pool.acquire,
+      idle: dbConfiguration.pool.idle,
+    },
+  }
+);
+
+const db = {};
+
+db.Sequelize = Sequelize;
+db.sequelize = sequelize;
+
+db.coffeejournals = require("./coffeejournal.model.js")(sequelize, Sequelize);
+
+module.exports = db;
