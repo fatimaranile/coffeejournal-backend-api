af7b2aca4342828fc11b37dfa1fd38989acda04a
Pat Ranile <fatimaranile@gmail.com>
2021-07-27 22:44:01 +0800

    Update route. Add the route for find all method.

--- app/routes/user.routes.js
+++ app/routes/user.routes.js
@@ -2,7 +2,10 @@ module.exports = (app) => {
   const users = require("../controllers/user.controller.js");
   var router = require("express").Router();
 
+  // create new user
   router.post("/", users.create);
+  // retrieve all users
+  router.get("/", users.findAll);
 
-  app.use("/users/create", router);
+  app.use("/coffeejournal/users", router);
 };
