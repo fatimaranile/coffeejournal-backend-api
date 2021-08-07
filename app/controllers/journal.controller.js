8e28b4dc6dfc64d701115b1b905392c66da279f9
Pat Ranile <fatimaranile@gmail.com>
2021-08-07 18:53:36 +0800

    Implement adding coffee journal.

--- app/controllers/journal.controller.js
+++ app/controllers/journal.controller.js
@@ -4,6 +4,50 @@ const Journal = db.journals;
 
 const Op = db.Sequelize.Op;
 
+exports.addJournal = (req, res) => {
+  // validate request
+  const {
+    coffeebean,
+    origin,
+    process,
+    roastdate,
+    brewingmethod,
+    yield,
+    notes,
+    rate,
+    userId,
+  } = req.body;
+  if (!coffeebean || !userId) {
+    res.status(400).send({
+      message: "Coffee bean and user ID cannot be empty!",
+    });
+
+    return;
+  }
+
+  const journal = {
+    coffeebean,
+    origin,
+    process,
+    roastdate,
+    brewingmethod,
+    yield,
+    notes,
+    rate,
+    userId,
+  };
+
+  Journal.create(journal)
+    .then((data) => {
+      res.send(data);
+    })
+    .catch((err) => {
+      res.status(500).send({
+        message: err.message || "An error occured while adding your journal.",
+      });
+    });
+};
+
 exports.findAll = (req, res) => {
   Journal.findAll()
     .then((data) => {
--- app/routes/journal.routes.js
+++ app/routes/journal.routes.js
@@ -4,4 +4,7 @@ var router = require("express").Router();
 // retrieve all journals
 router.get("/", journal.findAll);
 
+// add journal
+router.post("/add", journal.addJournal);
+
 module.exports = router;
